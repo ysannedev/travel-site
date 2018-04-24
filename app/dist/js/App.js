1168b044010f990e38a26db79fed203fa7d0455b
Ysanne Carr <ysanne.carr@gmail.com>
2018-04-24 20:00:51 +0200

    saving before merge w lazy loading?

--- app/dist/js/App.js
+++ app/dist/js/App.js
@@ -10535,6 +10535,7 @@ var StickyHeader = function () {
     function StickyHeader() {
         _classCallCheck(this, StickyHeader);
 
+        this.lazyImages = (0, _jquery2.default)('.lazyload');
         this.siteHeader = (0, _jquery2.default)('.site-header');
         this.siteHeaderLinks = (0, _jquery2.default)('.primary-nav a');
         this.pageSections = (0, _jquery2.default)('.page-section');
@@ -10542,9 +10543,17 @@ var StickyHeader = function () {
         this.createHeaderWaypoint();
         this.highlightHeaderLinks();
         this.addSmoothScroll();
+        this.refreshWaypoints();
     }
 
     _createClass(StickyHeader, [{
+        key: 'refreshWaypoints',
+        value: function refreshWaypoints() {
+            this.lazyImages.on('load', function () {
+                Waypoint.refreshAll();
+            });
+        }
+    }, {
         key: 'addSmoothScroll',
         value: function addSmoothScroll() {
             this.siteHeaderLinks.smoothScroll();
